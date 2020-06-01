527e8ff3e3801e611703bf407fb10cac4a462fa9
SarahMohamedAbdAlkader <44445357+SarahMohamedAbdAlkader@users.noreply.github.com>
2020-06-02 00:18:06 +0200

    With App.apk

---
+++ App/lib/main.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:sensors/sensors.dart';
+
+import 'Components/CurrentReadingComponent/currentReading.dart';
+import 'Components/CenteredComponent/centeredComponent.dart';
+import 'Components/LastComponent/LastReading.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Speedometer App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  double x, y, z;
+  int _speed = 0;
+  void setCurrentSpeed(int speed) {
+    setState(() {
+      _speed = speed;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    userAccelerometerEvents.listen((UserAccelerometerEvent event) {
+      setState(() {
+        x = event.x;
+        y = event.y;
+        z = event.z;
+        setCurrentSpeed(y.abs().round());
+      });
+    }); //get the sensor data and set then to the data types
+  }
+
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    var reading = CurrentReading(((x.abs()) * 1000).toStringAsFixed(2));
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            reading,
+            CenteredReading(((x.abs()) * 1000)),
+            LastReading((x.abs()) * 1000),
+
+            //Using buttons to increase an decrease speed
+            //           FloatingActionButton(
+            //   onPressed: _incrementCounter,
+            //   tooltip: 'Increment',
+            //   child: Icon(Icons.add),
+            // ),
+            // FloatingActionButton(
+            //   onPressed: _decrementCounter,
+            //   tooltip: 'Decrease',
+            //   child: Icon(Icons.remove),
+            // ),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
